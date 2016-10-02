f7bb51be0dec7cb48c00d17f27a4146c0d3cd9a5
David <hku.soft@gmail.com>
2016-10-02 12:23:09 +0800

    Changed the UI layout to vertical. Used Splitter control in place of table layout.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -32,8 +32,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.InputListBox = new System.Windows.Forms.ListBox();
             this.OutputListBox = new System.Windows.Forms.ListBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.MiddleToolbar = new System.Windows.Forms.ToolStrip();
             this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
             this.TrimAfterColon = new System.Windows.Forms.ToolStripButton();
             this.TrimAfterComma = new System.Windows.Forms.ToolStripButton();
@@ -41,16 +40,30 @@
             this.TrimAfterSemiColon = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.CustomWordsCombo = new System.Windows.Forms.ToolStripComboBox();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.TopToolbar = new System.Windows.Forms.ToolStrip();
             this.ClearInput = new System.Windows.Forms.ToolStripButton();
             this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
             this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.RemoveCustomWord = new System.Windows.Forms.ToolStripButton();
             this.TrimAfterCustomString = new System.Windows.Forms.ToolStripButton();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.toolStrip2.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
+            this.Root = new System.Windows.Forms.SplitContainer();
+            this.Bottom = new System.Windows.Forms.SplitContainer();
+            this.Top = new System.Windows.Forms.SplitContainer();
+            this.MiddleToolbar.SuspendLayout();
+            this.TopToolbar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            this.Root.Panel1.SuspendLayout();
+            this.Root.Panel2.SuspendLayout();
+            this.Root.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Bottom)).BeginInit();
+            this.Bottom.Panel1.SuspendLayout();
+            this.Bottom.Panel2.SuspendLayout();
+            this.Bottom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Top)).BeginInit();
+            this.Top.Panel1.SuspendLayout();
+            this.Top.Panel2.SuspendLayout();
+            this.Top.SuspendLayout();
             this.SuspendLayout();
             // 
             // InputListBox
@@ -59,10 +72,10 @@
             this.InputListBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.InputListBox.FormattingEnabled = true;
             this.InputListBox.ItemHeight = 21;
-            this.InputListBox.Location = new System.Drawing.Point(3, 36);
+            this.InputListBox.Location = new System.Drawing.Point(0, 0);
             this.InputListBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.InputListBox.Name = "InputListBox";
-            this.InputListBox.Size = new System.Drawing.Size(527, 521);
+            this.InputListBox.Size = new System.Drawing.Size(782, 274);
             this.InputListBox.TabIndex = 0;
             this.InputListBox.SelectedIndexChanged += new System.EventHandler(this.InputListBox_SelectedIndexChanged);
             this.InputListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.InputListBox_MouseDoubleClick);
@@ -73,37 +86,19 @@
             this.OutputListBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.OutputListBox.FormattingEnabled = true;
             this.OutputListBox.ItemHeight = 21;
-            this.OutputListBox.Location = new System.Drawing.Point(536, 36);
+            this.OutputListBox.Location = new System.Drawing.Point(0, 0);
             this.OutputListBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.OutputListBox.Name = "OutputListBox";
-            this.OutputListBox.Size = new System.Drawing.Size(528, 521);
+            this.OutputListBox.Size = new System.Drawing.Size(782, 207);
             this.OutputListBox.TabIndex = 1;
             this.toolTip1.SetToolTip(this.OutputListBox, "Trim words after custom string\\n in the left combo");
             this.OutputListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.OutputListBox_MouseDoubleClick);
             // 
-            // tableLayoutPanel1
+            // MiddleToolbar
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.toolStrip2, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.InputListBox, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.OutputListBox, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.toolStrip1, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1067, 561);
-            this.tableLayoutPanel1.TabIndex = 3;
-            // 
-            // toolStrip2
-            // 
-            this.toolStrip2.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(28, 28);
-            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MiddleToolbar.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.MiddleToolbar.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.MiddleToolbar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripLabel1,
             this.TrimAfterColon,
             this.TrimAfterComma,
@@ -113,11 +108,11 @@
             this.CustomWordsCombo,
             this.TrimAfterCustomString,
             this.RemoveCustomWord});
-            this.toolStrip2.Location = new System.Drawing.Point(533, 0);
-            this.toolStrip2.Name = "toolStrip2";
-            this.toolStrip2.Size = new System.Drawing.Size(534, 32);
-            this.toolStrip2.TabIndex = 3;
-            this.toolStrip2.Text = "toolStrip2";
+            this.MiddleToolbar.Location = new System.Drawing.Point(0, 0);
+            this.MiddleToolbar.Name = "MiddleToolbar";
+            this.MiddleToolbar.Size = new System.Drawing.Size(782, 39);
+            this.MiddleToolbar.TabIndex = 3;
+            this.MiddleToolbar.Text = "toolStrip2";
             // 
             // toolStripLabel1
             // 
@@ -125,7 +120,7 @@
             this.toolStripLabel1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.toolStripLabel1.Margin = new System.Windows.Forms.Padding(0, 6, 2, 2);
             this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(189, 24);
+            this.toolStripLabel1.Size = new System.Drawing.Size(189, 31);
             this.toolStripLabel1.Text = "Double Click to rename";
             this.toolStripLabel1.ToolTipText = "Double Click an item to rename with that string";
             // 
@@ -137,7 +132,7 @@
             this.TrimAfterColon.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.TrimAfterColon.Margin = new System.Windows.Forms.Padding(5, 5, 0, 2);
             this.TrimAfterColon.Name = "TrimAfterColon";
-            this.TrimAfterColon.Size = new System.Drawing.Size(34, 25);
+            this.TrimAfterColon.Size = new System.Drawing.Size(34, 32);
             this.TrimAfterColon.Text = "■:";
             this.TrimAfterColon.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.TrimAfterColon.ToolTipText = "Trim string after :";
@@ -151,7 +146,7 @@
             this.TrimAfterComma.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.TrimAfterComma.Margin = new System.Windows.Forms.Padding(5, 5, 0, 2);
             this.TrimAfterComma.Name = "TrimAfterComma";
-            this.TrimAfterComma.Size = new System.Drawing.Size(34, 25);
+            this.TrimAfterComma.Size = new System.Drawing.Size(34, 32);
             this.TrimAfterComma.Text = "■,";
             this.TrimAfterComma.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.TrimAfterComma.ToolTipText = "Trim string after ,";
@@ -165,7 +160,7 @@
             this.TrimAfterHyphen.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.TrimAfterHyphen.Margin = new System.Windows.Forms.Padding(5, 5, 0, 2);
             this.TrimAfterHyphen.Name = "TrimAfterHyphen";
-            this.TrimAfterHyphen.Size = new System.Drawing.Size(36, 25);
+            this.TrimAfterHyphen.Size = new System.Drawing.Size(36, 32);
             this.TrimAfterHyphen.Text = "■-";
             this.TrimAfterHyphen.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.TrimAfterHyphen.ToolTipText = "Trim string after -";
@@ -179,7 +174,7 @@
             this.TrimAfterSemiColon.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.TrimAfterSemiColon.Margin = new System.Windows.Forms.Padding(5, 5, 0, 2);
             this.TrimAfterSemiColon.Name = "TrimAfterSemiColon";
-            this.TrimAfterSemiColon.Size = new System.Drawing.Size(34, 25);
+            this.TrimAfterSemiColon.Size = new System.Drawing.Size(34, 32);
             this.TrimAfterSemiColon.Text = "■;";
             this.TrimAfterSemiColon.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.TrimAfterSemiColon.ToolTipText = "Trim string after ;";
@@ -188,27 +183,27 @@
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 32);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 39);
             // 
             // CustomWordsCombo
             // 
             this.CustomWordsCombo.Name = "CustomWordsCombo";
-            this.CustomWordsCombo.Size = new System.Drawing.Size(100, 32);
+            this.CustomWordsCombo.Size = new System.Drawing.Size(100, 39);
             this.CustomWordsCombo.Text = "toolStripButton2";
             this.CustomWordsCombo.KeyUp += new System.Windows.Forms.KeyEventHandler(this.CustomWordsCombo_KeyUp);
             // 
-            // toolStrip1
+            // TopToolbar
             // 
-            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TopToolbar.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.TopToolbar.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.TopToolbar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ClearInput,
             this.toolStripLabel2,
             this.toolStripLabel3});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(533, 31);
-            this.toolStrip1.TabIndex = 2;
+            this.TopToolbar.Location = new System.Drawing.Point(0, 0);
+            this.TopToolbar.Name = "TopToolbar";
+            this.TopToolbar.Size = new System.Drawing.Size(782, 31);
+            this.TopToolbar.TabIndex = 2;
             // 
             // ClearInput
             // 
@@ -244,7 +239,7 @@
             this.RemoveCustomWord.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.RemoveCustomWord.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.RemoveCustomWord.Name = "RemoveCustomWord";
-            this.RemoveCustomWord.Size = new System.Drawing.Size(36, 29);
+            this.RemoveCustomWord.Size = new System.Drawing.Size(36, 36);
             this.RemoveCustomWord.Text = "toolStripButton1";
             this.RemoveCustomWord.Click += new System.EventHandler(this.RemoveCustomWord_Click);
             // 
@@ -255,17 +250,73 @@
             this.TrimAfterCustomString.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.TrimAfterCustomString.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.TrimAfterCustomString.Name = "TrimAfterCustomString";
-            this.TrimAfterCustomString.Size = new System.Drawing.Size(36, 29);
+            this.TrimAfterCustomString.Size = new System.Drawing.Size(36, 36);
             this.TrimAfterCustomString.ToolTipText = "Trim words after \r\nCustom String\r\nShown in left combo\r\n<----\r\n";
             this.TrimAfterCustomString.Click += new System.EventHandler(this.TrimAfterCustomString_Click);
+            // 
+            // Root
+            // 
+            this.Root.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Root.Location = new System.Drawing.Point(0, 0);
+            this.Root.Name = "Root";
+            this.Root.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // Root.Panel1
+            // 
+            this.Root.Panel1.Controls.Add(this.Top);
+            // 
+            // Root.Panel2
+            // 
+            this.Root.Panel2.Controls.Add(this.Bottom);
+            this.Root.Size = new System.Drawing.Size(782, 553);
+            this.Root.SplitterDistance = 305;
+            this.Root.TabIndex = 4;
+            // 
+            // Bottom
+            // 
+            this.Bottom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Bottom.IsSplitterFixed = true;
+            this.Bottom.Location = new System.Drawing.Point(0, 0);
+            this.Bottom.Name = "Bottom";
+            this.Bottom.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // Bottom.Panel1
+            // 
+            this.Bottom.Panel1.Controls.Add(this.MiddleToolbar);
+            // 
+            // Bottom.Panel2
+            // 
+            this.Bottom.Panel2.Controls.Add(this.OutputListBox);
+            this.Bottom.Size = new System.Drawing.Size(782, 244);
+            this.Bottom.SplitterDistance = 33;
+            this.Bottom.TabIndex = 5;
+            // 
+            // Top
+            // 
+            this.Top.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Top.IsSplitterFixed = true;
+            this.Top.Location = new System.Drawing.Point(0, 0);
+            this.Top.Name = "Top";
+            this.Top.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // Top.Panel1
+            // 
+            this.Top.Panel1.Controls.Add(this.TopToolbar);
+            // 
+            // Top.Panel2
+            // 
+            this.Top.Panel2.Controls.Add(this.InputListBox);
+            this.Top.Size = new System.Drawing.Size(782, 305);
+            this.Top.SplitterDistance = 27;
+            this.Top.TabIndex = 5;
             // 
             // MainForm
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 561);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.Root);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
@@ -277,12 +328,24 @@
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
-            this.toolStrip2.ResumeLayout(false);
-            this.toolStrip2.PerformLayout();
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
+            this.MiddleToolbar.ResumeLayout(false);
+            this.MiddleToolbar.PerformLayout();
+            this.TopToolbar.ResumeLayout(false);
+            this.TopToolbar.PerformLayout();
+            this.Root.Panel1.ResumeLayout(false);
+            this.Root.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            this.Root.ResumeLayout(false);
+            this.Bottom.Panel1.ResumeLayout(false);
+            this.Bottom.Panel1.PerformLayout();
+            this.Bottom.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Bottom)).EndInit();
+            this.Bottom.ResumeLayout(false);
+            this.Top.Panel1.ResumeLayout(false);
+            this.Top.Panel1.PerformLayout();
+            this.Top.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Top)).EndInit();
+            this.Top.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -291,9 +354,8 @@
 
         private System.Windows.Forms.ListBox InputListBox;
         private System.Windows.Forms.ListBox OutputListBox;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.ToolStrip toolStrip2;
-        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStrip MiddleToolbar;
+        private System.Windows.Forms.ToolStrip TopToolbar;
         private System.Windows.Forms.ToolStripButton ClearInput;
         private System.Windows.Forms.ToolStripLabel toolStripLabel1;
         private System.Windows.Forms.ToolStripLabel toolStripLabel2;
@@ -307,6 +369,9 @@
         private System.Windows.Forms.ToolStripComboBox CustomWordsCombo;
         private System.Windows.Forms.ToolStripButton RemoveCustomWord;
         private System.Windows.Forms.ToolStripButton TrimAfterCustomString;
+        private System.Windows.Forms.SplitContainer Root;
+        private System.Windows.Forms.SplitContainer Bottom;
+        private System.Windows.Forms.SplitContainer Top;
     }
 }
 
